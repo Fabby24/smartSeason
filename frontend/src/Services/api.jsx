098d48999902ddd2e76f654736f3f3b0bebb784d
Fabby24 <musaufabian7@gmail.com>
2026-04-25 12:04:54 +0300

    final backend url

--- frontend/src/Services/api.jsx
+++ frontend/src/Services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'smartseason-production-151b.up.railway.app',
+    baseURL: 'https://smartseason-production-151b.up.railway.app/api',
     headers: {
         'Content-Type': 'application/json'
     }
